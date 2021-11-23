60169fc23da155fc07a2df30212e6f216ce72231
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-11-22 19:25:58 -0500

    Task 2 done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor """
+import math
+
+
+def floor(numb: float) -> int:
+    """ Ffunction floor: takes a float numb as argument """
+    return math.floor(numb)
