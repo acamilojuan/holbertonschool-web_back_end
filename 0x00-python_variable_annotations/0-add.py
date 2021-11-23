130a645dcc662c0d6fec7e6b7c0bd486b98197d9
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-11-22 19:13:42 -0500

    Method created

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic annotations - add """
+
+
+def add(numb: float, numb2: float) -> float:
+    """
+    Function add that takes a float a and a float b as arguments 
+    and returns their sum as a float
+    """
+    return numb + numb2
