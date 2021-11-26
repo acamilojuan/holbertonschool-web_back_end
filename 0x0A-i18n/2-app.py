c228dc86828214545cc93590375d9286ce27d7c1
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-11-25 23:10:11 -0500

    Update 2-app.py

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
+""" 2. Get locale from request
 """
-app module
-"""
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel
-from typing import Optional
-import os
 
 app = Flask(__name__)
-
-
-class Config:
-    """
-    Configuration of Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale() -> Optional[str]:
+class Config(object):
+    """ configure available languages
     """
-    Return best match from accepted languages
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """ Hello method.
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ determine best match from supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def home() -> str:
-    """
-    Home page
-    """
-
-    return render_template("2-index.html")
-
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port="5000")
