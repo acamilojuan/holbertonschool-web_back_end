5bb113333800cf872156a48c0b3b7b4488f8d08a
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-03 20:37:31 -0500

    Task 10 done

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- the first by the second number or returns 0 if the second number is equal to 0.
+delimiter //
+CREATE FUNCTION SafeDiv (
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	END IF;
+
+	RETURN a / b;
+END;
+//
