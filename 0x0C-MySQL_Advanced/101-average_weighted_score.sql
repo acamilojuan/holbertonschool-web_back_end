15a8f0fb55deefdf7461d3094fd9b7f15ecb3a8a
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-03 20:44:07 -0500

    Advanced Task 2 done

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,12 @@
+-- SQL script that creates a stored procedure that computes and store info
+delimiter //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+	UPDATE users
+	SET average_score = (SELECT sum(p.weight * c.score) / sum(p.weight)
+						 FROM projects AS p
+						 INNER JOIN corrections AS c
+							ON p.id = c.project_id
+                         WHERE c.user_id = users.id);
+END;
+//
