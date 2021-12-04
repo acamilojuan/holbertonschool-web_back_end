d5c04054d8a5a986099b3534f739da4b02e48dc9
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-03 20:30:18 -0500

    Update 6-bonus.sql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,17 +1,12 @@
 -- SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
-DELIMITER //
-CREATE PRECEDES AddBonus(
-        IN user_id INT,
-        In project_name VARCHAR(255),
-        IN score float)
+DELIMITER |
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-        INSERT INTO projects (name)
-
-        SELECT project_name FROM projects
-        WHERE project_name NOT IN (SELECT name FROM projects);
-
-        SET @project_id = LAST_INSERT_ID();
-        INSERT INTO corrections (user_id, project_id, score)
-        VALUES (user_id, @project_id, score)
-END //
-DELIMITER ;
+DECLARE idProject INT;
+IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
+    INSERT INTO projects(name) VALUES(project_name);
+END IF;
+SET idProject = (SELECT id FROM projects WHERE name = project_name);
+INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, idProject, score);
+END;
+|
