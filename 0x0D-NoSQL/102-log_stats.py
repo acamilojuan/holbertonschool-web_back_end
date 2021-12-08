ae561fa4bf08f02d2604c2326f74d41ec66a9912
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-07 21:50:10 -0500

    Update 102-log_stats.py

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python3
-""" Improving 12-log_stats.py adding the top 10 of the most present IPs in nginx database logs """
+""" Improving 12-log_stats.py adding top 10 IPs in nginx database logs """
+from pymongo import MongoClient
 
 
-def top_students(mongo_collection):
-    """ FX to return all students sorted by average score """
+def display_information(mongo_collection):
+    """Provides some stats about Nginx logs"""
+    print("{} logs".format(mongo_collection.count_documents({})))
+    print("Methods:")
 
-    top_student = mongo_collection.aggregate([
-        {
-            "$project": {
-                "name": "$name",
-                "averageScore": {"$avg": "$topics.score"}
-            }
-        },
-        {"$sort": {"averageScore": -1}}
-    ])
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    return top_student
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method, mongo_collection.count_documents({"method": method})))
+
+    print("{} status check".format(mongo_collection.count_documents(
+        {"method": methods[0], "path": "/status"})))
+
+    print("IPs:")
+
+    pipeline = [
+        {"$group": {
+            "_id": "$ip",
+            "count": {"$sum": 1},
+        }},
+        {"$sort": {"count": -1}}
+    ]
+
+    top_ips = list(mongo_collection.aggregate(pipeline))
+
+    for ip in top_ips[:10]:
+        print("\t{}: {}".format(ip.get('_id'), ip.get('count')))
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    display_information(nginx_collection)
