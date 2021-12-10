0faef69197a7d8f9afeb8b7c9a809c884d8e630c
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-09 20:46:44 -0500

    Update 11-createEmployeesObject.js

--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-    return { [`${departmentName}`]: employees.map(x => x) };
+  return {
+    [departmentName]: employees,  // [...employees]
+  };
 }
