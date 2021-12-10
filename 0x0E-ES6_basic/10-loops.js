14f259aabe8204c72a3d49384d6089abc52eaab3
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-09 20:39:47 -0500

    Update 10-loops.js

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-    const array2 = array;
-    for (const value of array) {
-      const idx = array.indexOf(value);
-      array2[idx] = appendString + value;
-    }
-  
-    return array2;
+  // let idx = 0;
+  for (const value of array) {
+    const idx = array.indexOf(value); // from old code. Much better than counter.
+    array[idx] = appendString + value;
+    // idx += 1;
   }
+
+  return array;
+}
+/* eslint-enable no-param-reassign */
